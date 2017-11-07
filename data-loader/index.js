95abc20657536c0638526ff934e6c2e32db3a4b3
Steven Roberts <sroberts994@gmail.com>
2017-11-07 12:56:51 -0500

    Load box scores of particular game to file

--- data-loader/index.js
+++ data-loader/index.js
@@ -6,6 +6,14 @@ NBA.stats.playerStats({
     Season: "2016-17"
 })
 .then(
-    (data) => util.promisify(fs.writeFile)("players.json", JSON.stringify(data))
+    (data) => util.promisify(fs.writeFile)("players-data.json", JSON.stringify(data))
+)
+.catch(console.error);
+
+NBA.stats.boxScore({
+    GameID: "0021700146"
+})
+.then(
+    (data) => util.promisify(fs.writeFile)("scores-data.json", JSON.stringify(data, null, '\t'))
 )
 .catch(console.error);
