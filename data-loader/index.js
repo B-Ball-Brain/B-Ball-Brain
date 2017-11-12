46046436e6b74b3a14be907e7baea4d84f860d6a
Steven Roberts <sroberts994@gmail.com>
2017-11-12 16:17:49 -0500

    Load nba stats from 2016-17 season

--- data-loader/index.js
+++ data-loader/index.js
@@ -1,16 +1,20 @@
-const nba = require("nba");
-const util = require("util");
-const fs = require("fs");
+const nba = require('nba');
+const util = require('util');
+const fs = require('fs');
 
-const downloadLeagueGameLog = (file, options) => nba.stats.leagueGameLog(options)
+const downloadLeagueGameLog = (file, ...options) => nba.stats.leagueGameLog(Object.assign(...options))
     .then(
         (data) => util.promisify(fs.writeFile)(file, JSON.stringify(data, null, '\t'))
     );
 
+const baseOptions = {
+    Season: '2016-17'
+};
+
 Promise.all([
-    downloadLeagueGameLog("player-data.json", {PlayerOrTeam: "P"}),
-    downloadLeagueGameLog("team-data.json", {PlayerOrTeam: "T"})
+    downloadLeagueGameLog('player-data.json', baseOptions, {PlayerOrTeam: 'P'}),
+    downloadLeagueGameLog('team-data.json', baseOptions, {PlayerOrTeam: 'T'})
 ]).then(
-    () => console.log("Successfully loaded data"),
+    () => console.log('Successfully loaded data'),
     console.error
 );
