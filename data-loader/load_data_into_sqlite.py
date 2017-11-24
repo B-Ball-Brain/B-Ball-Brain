b7d0527145bb54da1a16a2f37670d6d93241f977
Mahesh Narayanamurthi <mahesh.n.murthi@gmail.com>
2017-11-24 14:13:12 -0500

    Committing code to handle player discrepancies, solve the constraint satisfaction problem of picking the best match first before picking anyone else, code to create and load data into database.

--- data-loader/load_data_into_sqlite.py
+++ data-loader/load_data_into_sqlite.py
@@ -1,7 +1,86 @@
+
 from __future__ import print_function
 import os
+import re
 import json
 import sqlite3
+
+
+def playerLineupCSAT(playerPositions):
+    """
+    Accepts a dictionary with the following keys
+    playerPositions                     = dict()
+    playerPositions['Guard']            = []
+    playerPositions['Center']           = []
+    playerPositions['Forward']          = []
+    playerPositions['Forward-Center']   = []
+    playerPositions['Center-Forward']   = []
+    playerPositions['Forward-Guard']    = []
+    playerPositions['Guard-Forward']    = []
+    playerPositions['']                 = []
+    """
+    # Remove excess players
+    if len(playerPositions['Forward']) > 2:
+        playerPositions[''].extend(playerPositions['Forward'][2:])
+        del playerPositions['Forward'][2:]
+                
+    if len(playerPositions['Center']) > 1:
+        playerPositions[''].extend(playerPositions['Center'][1:])
+        del playerPositions['Center'][1:]
+
+    if len(playerPositions['Guard']) > 2:
+        playerPositions[''].extend(playerPositions['Guard'][2:])
+        del playerPositions['Guard'][2:]
+                
+    # Add players as needed
+    while len(playerPositions['Forward']) < 2 and len(playerPositions['Forward-Center']) > 0:
+        playerPositions['Forward'].append(playerPositions['Forward-Center'].pop())
+                
+    while len(playerPositions['Forward']) < 2 and len(playerPositions['Forward-Guard']) > 0:
+        playerPositions['Forward'].append(playerPositions['Forward-Guard'].pop())
+
+    while len(playerPositions['Center']) < 1  and len(playerPositions['Center-Forward']) > 0:
+        playerPositions['Center'].append(playerPositions['Center-Forward'].pop())
+
+    while len(playerPositions['Guard']) < 2  and len(playerPositions['Guard-Forward']) > 0:
+        playerPositions['Guard'].append(playerPositions['Guard-Forward'].pop())
+
+    while len(playerPositions['Forward']) < 2  and len(playerPositions['Center-Forward']) > 0:
+        playerPositions['Forward'].append(playerPositions['Center-Forward'].pop())
+
+    while len(playerPositions['Forward']) < 2  and len(playerPositions['Guard-Forward']) > 0:
+        playerPositions['Forward'].append(playerPositions['Guard-Forward'].pop())
+
+    while len(playerPositions['Center']) < 1 and len(playerPositions['Forward-Center']) > 0:
+        playerPositions['Center'].append(playerPositions['Forward-Center'].pop())
+                
+    while len(playerPositions['Guard']) < 2 and len(playerPositions['Forward-Guard']) > 0:
+        playerPositions['Guard'].append(playerPositions['Forward-Guard'].pop())
+
+    # Now pop everything into the empty key list
+    playerPositions[''].extend(playerPositions['Forward-Center'])
+    del playerPositions['Forward-Center'][0:]
+    playerPositions[''].extend(playerPositions['Forward-Guard'])
+    del playerPositions['Forward-Guard'][0:]
+    playerPositions[''].extend(playerPositions['Center-Forward'])
+    del playerPositions['Center-Forward'][0:]
+    playerPositions[''].extend(playerPositions['Guard-Forward'])
+    del playerPositions['Guard-Forward'][0:]
+
+    while len(playerPositions['Forward']) < 2 and len(playerPositions['']) > 0:
+        playerPositions['Forward'].append(playerPositions[''].pop())
+
+    while len(playerPositions['Center']) < 1  and len(playerPositions['']) > 0:
+        playerPositions['Center'].append(playerPositions[''].pop())
+                
+    while len(playerPositions['Guard']) < 2  and len(playerPositions['']) > 0:
+        playerPositions['Guard'].append(playerPositions[''].pop())
+
+    assert(len(playerPositions['Forward']) == 2)
+    assert(len(playerPositions['Center'])  == 1)
+    assert(len(playerPositions['Guard'])   == 2)
+
+
 
 # Open the database connection
 conn = sqlite3.connect('nba-insight.db')
@@ -16,72 +95,72 @@ curr.execute('DROP TABLE IF EXISTS playerstats')
 create_player_statistics_table = '''
 CREATE TABLE IF NOT EXISTS playerstats
 (
-"playerId" INTEGER PRIMARY KEY,
-"playerName" TEXT,
-"teamId" INTEGER,
-"teamAbbreviation" TEXT,
-"age" INTEGER,
-"gp" INTEGER,
-"w" INTEGER,
-"l" INTEGER,
-"wPct" REAL,
-"min" REAL,
-"fgm" REAL,
-"fga" REAL,
-"fgPct" REAL,
-"fG3M" INTEGER,
-"fG3A" INTEGER,
-"fg3Pct" INTEGER,
-"ftm" REAL,
-"fta" REAL,
-"ftPct" REAL,
-"oreb" REAL,
-"dreb" REAL,
-"reb" REAL,
-"ast" REAL,
-"tov" REAL,
-"stl" REAL,
-"blk" REAL,
-"blka" REAL,
-"pf" REAL,
-"pfd" REAL,
-"pts" REAL,
-"plusMinus" REAL,
-"nbaFantasyPts" REAL,
-"dD2" INTEGER,
-"tD3" INTEGER,
-"gpRank" INTEGER,
-"wRank" INTEGER,
-"lRank" INTEGER,
-"wPctRank" INTEGER,
-"minRank" INTEGER,
-"fgmRank" INTEGER,
-"fgaRank" INTEGER,
-"fgPctRank" INTEGER,
-"fg3mRank" INTEGER,
-"fg3aRank" INTEGER,
-"fg3PctRank" INTEGER,
-"ftmRank" INTEGER,
-"ftaRank" INTEGER,
-"ftPctRank" INTEGER,
-"orebRank" INTEGER,
-"drebRank" INTEGER,
-"rebRank" INTEGER,
-"astRank" INTEGER,
-"tovRank" INTEGER,
-"stlRank" INTEGER,
-"blkRank" INTEGER,
-"blkaRank" INTEGER,
-"pfRank" INTEGER,
-"pfdRank" INTEGER,
-"ptsRank" INTEGER,
-"plusMinusRank" INTEGER,
-"nbaFantasyPtsRank" INTEGER,
-"dd2Rank" INTEGER,
-"td3Rank" INTEGER,
-"cfid" INTEGER,
-"cfparams" TEXT,
-"position" TEXT);
+"playerId"           INTEGER PRIMARY KEY,
+"playerName"         TEXT,
+"teamId"             INTEGER,
+"teamAbbreviation"   TEXT,
+"age"                INTEGER,
+"gp"                 INTEGER,
+"w"                  INTEGER,
+"l"                  INTEGER,
+"wPct"               REAL,
+"min"                REAL,
+"fgm"                REAL,
+"fga"                REAL,
+"fgPct"              REAL,
+"fG3M"               INTEGER,
+"fG3A"               INTEGER,
+"fg3Pct"             INTEGER,
+"ftm"                REAL,
+"fta"                REAL,
+"ftPct"              REAL,
+"oreb"               REAL,
+"dreb"               REAL,
+"reb"                REAL,
+"ast"                REAL,
+"tov"                REAL,
+"stl"                REAL,
+"blk"                REAL,
+"blka"               REAL,
+"pf"                 REAL,
+"pfd"                REAL,
+"pts"                REAL,
+"plusMinus"          REAL,
+"nbaFantasyPts"      REAL,
+"dD2"                INTEGER,
+"tD3"                INTEGER,
+"gpRank"             INTEGER,
+"wRank"              INTEGER,
+"lRank"              INTEGER,
+"wPctRank"           INTEGER,
+"minRank"            INTEGER,
+"fgmRank"            INTEGER,
+"fgaRank"            INTEGER,
+"fgPctRank"          INTEGER,
+"fg3mRank"           INTEGER,
+"fg3aRank"           INTEGER,
+"fg3PctRank"         INTEGER,
+"ftmRank"            INTEGER,
+"ftaRank"            INTEGER,
+"ftPctRank"          INTEGER,
+"orebRank"           INTEGER,
+"drebRank"           INTEGER,
+"rebRank"            INTEGER,
+"astRank"            INTEGER,
+"tovRank"            INTEGER,
+"stlRank"            INTEGER,
+"blkRank"            INTEGER,
+"blkaRank"           INTEGER,
+"pfRank"             INTEGER,
+"pfdRank"            INTEGER,
+"ptsRank"            INTEGER,
+"plusMinusRank"      INTEGER,
+"nbaFantasyPtsRank"  INTEGER,
+"dd2Rank"            INTEGER,
+"td3Rank"            INTEGER,
+"cfid"               INTEGER,
+"cfparams"           TEXT,
+"position"           TEXT);
 '''
 
 curr.execute(create_player_statistics_table)
@@ -104,6 +183,133 @@ with open('output/player-stats.json','r') as f:
 
 conn.commit()
 
+for row in curr.execute("SELECT DISTINCT(position) from playerstats"):
+    print(row[0])
 
+# Drop table if it exists
+curr.execute('DROP TABLE IF EXISTS playermatchup')
 
+# Create a table if it does not exist for player statistics
+create_player_matchup_table = '''
+CREATE TABLE IF NOT EXISTS playermatchup
+(
+id                     INTEGER PRIMARY KEY AUTOINCREMENT,       
+gameId                 INTEGER,          
+startTime              INTEGER, 
+endTime                INTEGER,         
+homeTeamCenterId       INTEGER,          
+homeTeamForward1Id     INTEGER,         
+homeTeamForward2Id     INTEGER,          
+homeTeamGuard1Id       INTEGER,     
+homeTeamGuard2Id       INTEGER,       
+awayTeamCenterId       INTEGER,      
+awayTeamForward1Id     INTEGER,       
+awayTeamForward2Id     INTEGER,      
+awayTeamGuard1Id       INTEGER,    
+awayTeamGuard2Id       INTEGER,   
+plusMinusPerMinute     REAL          
+);                    
+'''                   
+
+# Create table to store player match ups
+curr.execute(create_player_matchup_table)
+
+# RegEx to match timecapsule filenames
+timecapsule_filename_regex = re.compile('[0-9]+-[0-9]+-[0-9]+-data\.json')
+
+for filename in os.listdir('output/'):
+    if timecapsule_filename_regex.match(filename):
+        with open('output/' + filename, 'r') as f:
+            data                                 = json.load(f)
+            values_dict                          = dict()
+            values_dict["gameId"]                = data["gameId"]     
+            values_dict["startTime"]             = data["startTime"]
+            values_dict["endTime"]               = data["endTime"]   
+            values_dict["homeTeamCenterId"]      = None    
+            values_dict["homeTeamForward1Id"]    = None
+            values_dict["homeTeamForward2Id"]    = None    
+            values_dict["homeTeamGuard1Id"]      = None 
+            values_dict["homeTeamGuard2Id"]      = None 
+            values_dict["awayTeamCenterId"]      = None
+            values_dict["awayTeamForward1Id"]    = None
+            values_dict["awayTeamForward2Id"]    = None
+            values_dict["awayTeamGuard1Id"]      = None
+            values_dict["awayTeamGuard2Id"]      = None
+            values_dict["plusMinusPerMinute"]    = data["pmPerMinute"]
+
+            # Make sure that IDs are not repeating
+            assert(len(set(data["homePlayers"])) == 5 and len(data["homePlayers"]) == 5)
+            assert(len(set(data["awayPlayers"])) == 5 and len(data["awayPlayers"]) == 5)
+            assert(len(set(data["homePlayers"] + data["awayPlayers"])) == 10)
+                                                   
+            # now go over the home team players and query what
+            # their position is and then try to satisfy consts.
+            # List of player positions
+            playerPositions                     = dict()
+            playerPositions['Guard']            = []
+            playerPositions['Center']           = []
+            playerPositions['Forward']          = []
+            playerPositions['Forward-Center']   = []
+            playerPositions['Center-Forward']   = []
+            playerPositions['Forward-Guard']    = []
+            playerPositions['Guard-Forward']    = []
+            playerPositions['']                 = []
+            
+            for playerId in data["homePlayers"]:
+                # Query home player and add to player positions
+                positionSql = "SELECT position FROM playerstats WHERE playerId = {}".format(playerId)
+                for position in curr.execute(positionSql):
+                    for key in playerPositions.keys():
+                        if key == position[0].strip():
+                            if playerId not in playerPositions[position[0]]:
+                                playerPositions[position[0]].append(playerId)
+
+            # Now solve the constraint satisfaction problem
+            playerLineupCSAT(playerPositions)
+
+            # Fill the values dict now
+            values_dict["homeTeamCenterId"]      = playerPositions["Center"][0]    
+            values_dict["homeTeamForward1Id"]    = playerPositions["Forward"][0]    
+            values_dict["homeTeamForward2Id"]    = playerPositions["Forward"][1]    
+            values_dict["homeTeamGuard1Id"]      = playerPositions["Guard"][0]    
+            values_dict["homeTeamGuard2Id"]      = playerPositions["Guard"][1]    
+
+            # now go over the away team players and query what
+            # their position is and then try to satisfy consts.
+            # List of player positions
+            playerPositions                     = dict()
+            playerPositions['Guard']            = []
+            playerPositions['Center']           = []
+            playerPositions['Forward']          = []
+            playerPositions['Forward-Center']   = []
+            playerPositions['Center-Forward']   = []
+            playerPositions['Forward-Guard']    = []
+            playerPositions['Guard-Forward']    = []
+            playerPositions['']                 = []
+            
+            for playerId in data["awayPlayers"]:
+                # Query home player and add to player positions
+                positionSql = "SELECT position FROM playerstats WHERE playerId = {}".format(playerId)
+                for position in curr.execute(positionSql):
+                    for key in playerPositions.keys():
+                        if key == position[0].strip():
+                            if playerId not in playerPositions[position[0]]:
+                                playerPositions[position[0]].append(playerId)
+            
+            # Now solve the constraint satisfaction problem    
+            playerLineupCSAT(playerPositions)
+
+            values_dict["awayTeamCenterId"]      = playerPositions["Center"][0]
+            values_dict["awayTeamForward1Id"]    = playerPositions["Forward"][0]
+            values_dict["awayTeamForward2Id"]    = playerPositions["Forward"][1]
+            values_dict["awayTeamGuard1Id"]      = playerPositions["Guard"][0]
+            values_dict["awayTeamGuard2Id"]      = playerPositions["Guard"][1]
+
+            # Now insert the record into the table
+            columns      = ', '.join(values_dict.keys())
+            placeholders = ', '.join('?' * len(values_dict))
+            insertsql    = '''INSERT INTO {}  ({}) VALUES ({})'''.format("playermatchup", columns, placeholders)
+            curr.execute(insertsql, values_dict.values())
+
+conn.commit()
 conn.close()
