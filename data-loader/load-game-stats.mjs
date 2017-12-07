ebc0f088102c67f7f6fef21df734770778603798
Steven Roberts <sroberts994@gmail.com>
2017-12-06 19:45:29 -0500

    Load data from the playoffs for testing

--- data-loader/load-game-stats.mjs
+++ data-loader/load-game-stats.mjs
@@ -1,31 +1,49 @@
 import 'colors';
-import NbaStats from './nba-stats';
+import {
+    loadGameIds,
+    loadGameSubTimes,
+    loadTrainingDataInTimeRange
+} from './nba-stats';
+import config from './config';
 import fs from 'fs';
+import makeDir from 'make-dir';
+import path from 'path';
 
 const playersOnCourtPerTeam = 5;
 
-const writeData = async (options) => {
-    const nbaStats = new NbaStats(options.season);
+const writeData = async (minTimeBetweenSubs, subPadding, options) => {
+    const outputDir = path.join(
+        config.outputDir,
+        options.Season,
+        options.SeasonType
+    );
+    await makeDir(outputDir);
 
     console.log('Loading all game IDs');
-    const gameIds = await nbaStats.loadGameIds();
+    const gameIds = await loadGameIds(options);
 
     let i = 1;
     for (const gameId of gameIds) {
         console.log(`Loading game ${i} of ${gameIds.size}: ${gameId}`);
         i++;
 
-        const subTimes = await nbaStats.loadGameSubTimes(gameId);
+        const subTimes = await loadGameSubTimes(gameId, options);
 
         let startTime = 0;
         for (const subTime of subTimes) {
-            const endTime = subTime - options.subPadding;
-            const file = `output/${gameId}-${startTime}-${endTime}-data.json`;
+            const endTime = subTime - subPadding;
+            const outputFile = path.join(
+                outputDir, `${gameId}-${startTime}-${endTime}-data.json`
+            );
 
-            if (endTime - options.minTimeBetweenSubs > startTime &&
-                !fs.existsSync(file)) {
-                const timeRangeStats = await nbaStats
-                    .loadTrainingDataInTimeRange(gameId, startTime, endTime);
+            if (endTime - minTimeBetweenSubs > startTime &&
+                !fs.existsSync(outputFile)) {
+                const timeRangeStats = await loadTrainingDataInTimeRange(
+                    gameId,
+                    startTime,
+                    endTime,
+                    options
+                );
 
                 const numHomePlayers = timeRangeStats.homePlayers.length;
                 const numAwayPlayers = timeRangeStats.awayPlayers.length;
@@ -33,7 +51,7 @@ const writeData = async (options) => {
                 if (numHomePlayers === playersOnCourtPerTeam &&
                     numAwayPlayers === playersOnCourtPerTeam) {
                     fs.writeFileSync(
-                        file,
+                        outputFile,
                         JSON.stringify(timeRangeStats, null, '\t')
                     );
                 } else {
@@ -43,16 +61,30 @@ Skipping...`.yellow);
                 }
             }
 
-            startTime = subTime + options.subPadding;
+            startTime = subTime + subPadding;
         }
     }
 };
 
-writeData({
-    season: '2016-17',
-    minTimeBetweenSubs: 300,
-    subPadding: 10
-}).then(
-    () => console.log('Successfully wrote data'.green),
-    (err) => console.error(err.toString().red)
-);
+const run = async () => {
+    try {
+        const minTimeBetweenSubs = 300;
+        const subPadding = 10;
+
+        await writeData(minTimeBetweenSubs, subPadding, {
+            Season: config.season,
+            SeasonType: 'Regular Season'
+        });
+
+        await writeData(minTimeBetweenSubs, subPadding, {
+            Season: config.season,
+            SeasonType: 'Playoffs'
+        });
+
+        console.log('Successfully wrote data'.green);
+    } catch (err) {
+        console.error(err.toString().red);
+    }
+};
+
+run();
--- data-loader/load-player-stats.mjs
+++ data-loader/load-player-stats.mjs
@@ -1,13 +1,31 @@
 import 'colors';
-import NbaStats from './nba-stats';
+import {loadAvgPlayerStats, loadPlayerInfo} from './nba-stats';
+import config from './config';
 import fs from 'fs';
+import makeDir from 'make-dir';
+import path from 'path';
 
-const nbaStats = new NbaStats('2016-17');
-nbaStats.loadAvgPlayerStats()
-    .then((stats) => {
-        fs.writeFileSync(
-            'output/player-stats.json',
-            JSON.stringify(stats, null, '\t')
-        );
-    })
-    .catch((err) => console.log(err.toString().red));
+const run = async () => {
+    try {
+        const options = {
+            Season: config.season
+        };
+
+        const playerStats = await loadAvgPlayerStats(options);
+
+        for (const p of playerStats) {
+            const playerInfo = await loadPlayerInfo(p.playerId, options);
+            p.position = playerInfo.position;
+        }
+
+        const outputDir = path.join(config.outputDir, config.season);
+        await makeDir(outputDir);
+
+        const outputFile = path.join(outputDir, 'player-stats.json');
+        fs.writeFileSync(outputFile, JSON.stringify(playerStats, null, '\t'));
+    } catch (err) {
+        console.log(err.toString().red);
+    }
+};
+
+run();
