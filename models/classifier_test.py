690a20e2855386ccbbd8b15f4002e4aeae3fc964
Mahesh Narayanamurthi <mahesh.n.murthi@gmail.com>
2017-12-12 19:30:03 -0500

    Tuning model parameters

--- models/classifier_test.py
+++ models/classifier_test.py
@@ -18,20 +18,18 @@ from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 
-#TEST_PERCENT = .20
-
+# TEST_PERCENT = 0.2
 
 def getNewModel():
-
-    yield LogisticRegression()
-    yield LinearDiscriminantAnalysis()
-    yield KNeighborsClassifier()
-    #yield MLPClassifier()
-    yield GaussianNB()
-    yield SVC(kernel='linear')
-    #yield SVC(kernel='rbf')
-    yield GradientBoostingClassifier()
-    #yield RandomForestClassifier()
+    # yield LogisticRegression(C = 0.01, tol=1.0e-4)
+    # yield LinearDiscriminantAnalysis()
+    # yield KNeighborsClassifier(n_neighbors = 10, weights='distance')
+    # yield MLPClassifier(hidden_layer_sizes=(64, 12, 12, 8), solver = 'adam', activation='logistic', alpha=0.01, max_iter=400)
+    # yield GaussianNB()
+    yield SVC(kernel='linear', C = 1)
+    # yield SVC(kernel='rbf')
+    # yield GradientBoostingClassifier()
+    # yield RandomForestClassifier()
     
 def main():
     train_data, test_data, synthetic_data = fileUtilities.getDataFromCSV()
@@ -45,14 +43,17 @@ def main():
     testX = scaler.transform(testX)
     #trainX, testX, trainY, testY = train_test_split(x, y, test_size=TEST_PERCENT)
 
-    trainY[trainY >= 0] = 1
-    trainY[trainY < 0] = -1
-    testY[testY >= 0] = 1
-    testY[testY < 0] = -1
+    trainY[trainY >= 0] =  1
+    trainY[trainY < 0]  = -1
+    testY[testY >= 0]   =  1
+    testY[testY < 0]    = -1
+    
     print(trainY)
+    
     numFeatures = trainX.shape[1]
-    trainSize = trainX.shape[0]
-    testSize = testX.shape[0]
+    trainSize   = trainX.shape[0]
+    testSize    = testX.shape[0]
+
     assert trainSize > 50
     assert testSize > 0
     assert numFeatures > 0
@@ -68,14 +69,14 @@ def main():
             model.name = str(model).split('(')[0]
             trainModel(model, trainX, trainY)
             model_list.append(model)
-        print("\n")
+            print("\n")
         for model in model_list:
             testModel(model, testX, testY)
             printModelResults(model)
             outFile.write(getOutputModelString(model) + '\n')
-        print("\n")
-        for model in model_list:
-        	makeIndividualPredictions(model, synthetic_data)
+            print("\n")
+        # for model in model_list:
+        #     makeIndividualPredictions(model, synthetic_data)
 
 
 def trainModel(model, trainData, trainLabels):
@@ -84,17 +85,18 @@ def trainModel(model, trainData, trainLabels):
     start = time()
     model.fit(trainData, trainLabels)
     model.trainDuration = time() - start
-    trainPredictions = model.predict(trainData)
+    trainPredictions    = model.predict(trainData)
     model.trainAccuracy = accuracy_score(trainLabels, trainPredictions)
-    model.classReport = classification_report(trainLabels, trainPredictions)
+    model.classReport   = classification_report(trainLabels, trainPredictions)
 
 
 def testModel(model, testData, testLabels):
     print("Testing with", model.name, "...")
-    model.testSize = testData.shape[0]
+    model.testSize  = testData.shape[0]
     testPredictions = model.predict(testData)
+    print(testPredictions)
     model.testAccuracy = accuracy_score(testLabels, testPredictions)
-    model.confMatrix = confusion_matrix(testLabels, testPredictions)
+    model.confMatrix   = confusion_matrix(testLabels, testPredictions)
     print(classification_report(testLabels, testPredictions))
 
 
@@ -103,37 +105,31 @@ def printModelResults(model):
     estimators = model.get_params().get('n_estimators', 0)
     if estimators > 0:
         name += ", est=" + str(estimators)
-    dur = "{0:.3f}".format(model.trainDuration)
-    trainAccuracy = "{0:.3f}".format(model.trainAccuracy)
-    testAccuracy = "{0:.3f}".format(model.testAccuracy)
-    outputs = name, dur, trainAccuracy, testAccuracy
-    print("\t{} took {} secs with train accuracy of {} and test accuracy of {}".format(*outputs))
+        dur   = "{0:.3f}".format(model.trainDuration)
+        trainAccuracy = "{0:.3f}".format(model.trainAccuracy)
+        testAccuracy  = "{0:.3f}".format(model.testAccuracy)
+        outputs = name, dur, trainAccuracy, testAccuracy
+        print("\t{} took {} secs with train accuracy of {} and test accuracy of {}".format(*outputs))
 
 
 def getOutputModelString(m):
-    # headers = "Name,Alpha,Estimators,Duration,TrainSize,TrainMSE,TestSize,TestMSE,r^2"
-    # alphas = m.get_params().get('alphas', [])
     alpha = getattr(m, 'alpha_', '')
     estimator = m.get_params().get('n_estimators', '')
-    #fullName = m.name + str(estimator)
-    fullName = m.name
-    output = [fullName]
+    fullName  = m.name
+    output    = [fullName]
     output.append(m.trainDuration)
     output.append(m.trainSize)
     output.append(m.trainAccuracy)
-    #output.append(m.trainRMSE)
     output.append(m.testSize)
     output.append(m.testAccuracy)
-    #output.append(m.testRMSE)
-    #output.append(m.score)
     return ','.join([str(val) for val in output])
 
 
 def makeIndividualPredictions(model, s_data):
-	#testPredictions = model.predict(scaler.transform(s_data))
-	testPredictions = model.predict(s_data)
-	formattedPred = ["{:0.3f}".format(member) for member in testPredictions]
-	print("Testing synthetic data with", model.name, "gives the predictions", formattedPred)
+    #testPredictions = model.predict(scaler.transform(s_data))
+    testPredictions = model.predict(s_data)
+    formattedPred = ["{:0.3f}".format(member) for member in testPredictions]
+    print("Testing synthetic data with", model.name, "gives the predictions", formattedPred)
 
 
 if __name__ == '__main__':
